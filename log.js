5d107dade6c1c66731f393340bd9e9ada676b3e4
Adrien Schuler <adrien.schuler@native-instruments.de>
2024-07-04 15:19:12 +0200

    wip refacto in src folder

--- log.js
+++
@@ -1,7 +0,0 @@
-const stringifyData = (data) => (_.map(data, (item) => JSON.stringify(item))).join('\n')
-
-module.exports = {
-    debug: (...data) => {
-        console.log('[DEBUG]', Game.time, stringifyData(data));
-    }
-}
