2182ef8a9917e260d71877f29de91eef488fa713
Adrien Schuler <adrien.schuler@native-instruments.de>
2024-07-04 15:46:59 +0200

    fix: upgraders capacity

--- src/creep.upgrader.js
+++ src/creep.upgrader.js
@@ -2,7 +2,7 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()) {
+        if (creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
