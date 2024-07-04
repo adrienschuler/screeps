f365e19d0f937135484a649210517e193910dd52
Adrien Schuler <adrien.schuler@native-instruments.de>
2024-07-04 18:37:35 +0200

    feat: haulers transfer to extensions

--- src/creep.hauler.js
+++ src/creep.hauler.js
@@ -12,10 +12,17 @@ var hauler = {
             }
         }
         else {
-            const spawns = creep.room.find(FIND_MY_SPAWNS)
-            const closestSpawn = creep.pos.findClosestByRange(spawns)
+            // const spawns = creep.room.find(FIND_MY_SPAWNS);
+            // const closestSpawn = creep.pos.findClosestByRange(spawns);
 
-            if (creep.transfer(closestSpawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+
+            if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(closestSpawn, { visualizePathStyle: { stroke: '#ffaa00' } });
             }
         }
